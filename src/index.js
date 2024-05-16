c3703ad63c78372e0b65436c093347433c0732ff
肖佳坤 <xiao@xiaojiakundeMacBook-Pro.local>
2024-05-16 21:31:18 +0800

    feat: 解构配置&引入microapp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import microApp from '@micro-zoe/micro-app'
+
 import './index.css';
 import App from './App';
+
+microApp.start();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
