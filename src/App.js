60f31fcbb03d8ae8ee5a1931a901b0914381a8ac
xiaojiakun <xiaojiakun@isiiot.com>
2024-05-16 12:21:00 +0800

    feat: 接口调整

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {useRequest} from 'ahooks';
 
 function App() {
 const getData = async () => {
-  const res = await fetch('https://110.41.132.237:3000/test');
+  const res = await fetch('https://xiaojiakun.top/test/');
   const json = await res.json();
   return json?.a;
 };
