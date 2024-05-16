95f4b32e196ea475357e92af38114e0c09b28212
xiaojiakun <xiaojiakun@isiiot.com>
2024-05-16 15:13:57 +0800

    feat: 增加知识框架图链接

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ const getData = async () => {
 const { data, error, loading } = useRequest(getData);
 const link = (path) => {
   window.open(path);
-}
+};
+const docLink = 'https://docs.qq.com/doc/DZU11WXFheXBJcmFL?layoutKey=newdoc&createInMall=true&disableFilter=true&folder=&is_panel=1&panel_key=29c5da37528bde745e50babdec8e2169&panel_type=doc&panel_connection_type=2&is_preload=0&fromPage=doc_list_new&create_type=2&aid_position=templatemall&aid_pos=templatemall';
+
   return (
     <div className="App">
       <header className="App-header">
-        欢迎登陆xjk的前端学习站点
+        <span class="mb-20">欢迎登陆xjk的前端学习站点</span>
+        <content>
+          <span class="link" onClick={() => link(docLink)}>前端知识框架</span>
+        </content>
       </header>
       <footer className="App-footer">
         <span class="link" onClick={() => link('https://beian.miit.gov.cn/')}>赣ICP备2024033381号</span>
