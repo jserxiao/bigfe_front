febead550b8977c974d1bde0de953153e23d20c8
肖佳坤 <xiao@xiaojiakundeMacBook-Pro.local>
2024-05-18 14:12:48 +0800

    feat: 主应用修改

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
-import {useState, useEffect} from 'react';
+import {useState, useLayoutEffect,useRef,useMemo} from 'react';
 import {useRequest} from 'ahooks';
+import {apps} from './subapp.conf.ts';
+import { useSize } from 'ahooks';
 
 function App() {
-const getData = async () => {
-  const res = await fetch('https://xiaojiakun.top/test/');
-  const json = await res.json();
-  console.log(json?.a);
-  return json?.a;
-};
-const { data, error, loading } = useRequest(getData);
-const link = (path) => {
-  window.open(path);
-};
-const docLink = 'https://docs.qq.com/doc/DZU11WXFheXBJcmFL?layoutKey=newdoc&createInMall=true&disableFilter=true&folder=&is_panel=1&panel_key=29c5da37528bde745e50babdec8e2169&panel_type=doc&panel_connection_type=2&is_preload=0&fromPage=doc_list_new&create_type=2&aid_position=templatemall&aid_pos=templatemall';
+  const getData = async () => {
+    const res = await fetch('https://xiaojiakun.top/test/');
+    const json = await res.json();
+    console.log(json?.a);
+    return json?.a;
+  };
+  const { data, error, loading } = useRequest(getData);
+  const link = (path) => {
+    window.open(path);
+  };
+  const appContentRef = useRef();
+  const appContentSize = useSize(appContentRef);
+  const microAppStyle = useMemo(() => ({
+    height: appContentSize?.height
+  }));
+  useLayoutEffect(() => {
+    appContentRef.current = document.querySelector('.App-content');
+  }, [])
 
   return (
     <div className="App">
@@ -21,13 +30,13 @@ const docLink = 'https://docs.qq.com/doc/DZU11WXFheXBJcmFL?layoutKey=newdoc&crea
         <span class="mb-20">欢迎登陆xjk的前端学习站点</span>
       </header>
       <main className="App-content">
-        <span class="link" onClick={() => link(docLink)}>前端知识框架</span>
+       {apps.map(x => <micro-app class="microapp" name={x.name} url={x.url} style={microAppStyle}></micro-app>)}
       </main>
       <footer className="App-footer">
-        <span class="link" onClick={() => link('https://beian.miit.gov.cn/')}>赣ICP备2024033381号</span>
+        <span className="link" onClick={() => link('https://beian.miit.gov.cn/')}>赣ICP备2024033381号</span>
         <div style={{marginTop: '20px'}}>
-          <span class="frend-link">友情链接：</span>
-          <span class="frend-link link" onClick={() => link('https://sfengweb.com')}>随风sfeng</span>
+          <span className="frend-link">友情链接：</span>
+          <span className="frend-link link" onClick={() => link('https://sfengweb.com')}>随风sfeng</span>
         </div>
       </footer>
     </div>
---
+++ src/subapp.conf.ts
@@ -0,0 +1,5 @@
+const origin = 'https://xiaojiakun.top';
+export const apps = [
+    {name: 'subreact', url: `${origin}/subreact/`},
+    {name: 'subvue', url: `${origin}/subvue/`}
+]
