e627324b51cbcefd3483a99a63bf07dc873b6646
xiaojiakun <xiaojiakun@isiiot.com>
2024-05-22 11:35:14 +0800

    feat: modify sh脚本

--- gitpush.sh
+++ gitpush.sh
@@ -3,10 +3,16 @@
 # 添加所有变更到暂存区
 git add . & pid=$!
 wait $pid
+echo "<====================添加变更到暂存区成功====================>"
 # 提交变更
 git commit -m "$1" & pid=$!
 wait $pid
-git pull --rebase & pid=$!
-wait $pid
+echo "<====================commit成功====================>"
+if git pull --rebase : then
 # 推送到远程仓库
-git push origin "$2"
+git push origin "$2" & pid=$!
+wait $pid
+echo "代码推送成功"
+else 
+echo "代码拉取失败，可能存在冲突"
+fi
